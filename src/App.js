d556729bd921dc5921224d87d6d52ef0aa378789
vorobjovsh <vorobjov75@gmail.com>
2021-12-24 19:28:41 +0200

    add component TransactionHistory

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import Profile from './components/Profile/Profile';
 import Statistics from './components/Statistics/Statistics';
 import FriendList from './components/FriendList/FriendList';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
 import user from './user.json';
 import data from './data.json';
 import friends from './friends.json';
+import transactions from './transactions.json';
 
 function App() {
   return (
-    <div>
+    <div className="block">
       <Profile
         username={user.username}
         tag={user.tag}
@@ -22,6 +24,7 @@ function App() {
       <Statistics title="Upload stats" stats={data} />
       <Statistics stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 }
---
+++ src/components/TransactionHistory/TransactionHistory.js
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types';
+import s from './TransactionHistory.module.css';
+
+function TransactionHistory({ items }) {
+  return (
+    <table className={s.transaction_history}>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map(item => (
+          <tr key={item.id}>
+            <td>{item.type}</td>
+            <td>{item.amount}</td>
+            <td>{item.currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }),
+  ),
+};
+
+export default TransactionHistory;
